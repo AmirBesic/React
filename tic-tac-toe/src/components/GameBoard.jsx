a67503ffab338ffd8a31b56cd7de7ed1a736f4d7
AmirBesic <101523899+AmirBesic@users.noreply.github.com>
2023-11-24 21:38:39 +0100

    tic-tac-toe game

---
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -0,0 +1,34 @@
+const GameBoard = ({ onSelectSquare, board }) => {
+  // const handleSelectSquare = (rowIndex, colIndex) => {
+  //    setGameBoard((prevGameBoard) => {
+  //      const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+  //      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //      return updatedBoard
+  //    });
+  //   onSelectSquare()
+
+  // };
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+            ;
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
